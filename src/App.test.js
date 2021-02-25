53005b2cf48ccecfd5efd23d9b2f73653acc1be8
kherma <kamil.herma01@gmail.com>
2021-02-25 19:09:56 +0100

    Add eslint tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-it('renders render without crashing', () => {
+it("renders render without crashing", () => {
   expect(shallow(<App />)).toBeTruthy();
 });
